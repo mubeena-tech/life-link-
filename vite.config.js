1f3f21b115d7fa82a432b75771e44da8ec879aa1
Your Name <0509mubeena@gmail.com>
2026-09-22 00:58:04 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/life-1/',
+  base: '/life-link-/',
   plugins: [react()],
 })
